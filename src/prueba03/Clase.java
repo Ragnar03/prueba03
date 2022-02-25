d11056e1eb4338471565231167a0d92308c0daf6
Alumno2 <Alumno2@1ASIR-5>
2022-02-25 09:24:02 +0100

    Hola

---
+++ src/prueba03/Clase.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package prueba03;
+
+
+
+public class Clase {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+    
+}
+
--- src/prueba03/Prueba03.java
+++ src/prueba03/Prueba03.java
@@ -4,9 +4,6 @@ package prueba03;
 
 public class Prueba03 {
 
-    
-    public static void main(String[] args) {
-        
-    }
+   
     
 }
